b39f9ca1ae61c985f645830dfb80d394f6152596
uPlexa <uplexa@protonmail.com>
2019-04-20 23:42:11 +0200

    Light->upxtwo

--- src/crypto/hash.h
+++ src/crypto/hash.h
@@ -72,11 +72,11 @@ namespace crypto {
   }
 
   inline void cn_slow_hash(const void *data, std::size_t length, hash &hash, int upxtwo = 0, int variant = 0) {
-    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), light, variant, 0/*prehashed*/);
+    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), upxtwo, variant, 0/*prehashed*/);
   }
 
   inline void cn_slow_hash_prehashed(const void *data, std::size_t length, hash &hash, int upxtwo = 0, int variant = 0) {
-    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), light, variant, 1/*prehashed*/);
+    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), upxtwo, variant, 1/*prehashed*/);
   }
 
   inline void tree_hash(const hash *hashes, std::size_t count, hash &root_hash) {

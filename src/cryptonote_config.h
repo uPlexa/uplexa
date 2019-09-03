ffb61c660edda8a5aebc9ccd10a2d6f702ab5e07
Earl R. Lapus <earl.lapus@gmail.com>
2019-09-03 18:01:28 +0800

    add "pruning" definitions

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -155,6 +155,10 @@
 
 #define BULLETPROOF_MAX_OUTPUTS                 16
 
+#define CRYPTONOTE_PRUNING_STRIPE_SIZE          4096 // the smaller, the smoother the increase
+#define CRYPTONOTE_PRUNING_LOG_STRIPES          3 // the higher, the more space saved
+#define CRYPTONOTE_PRUNING_TIP_BLOCKS           5500 // the smaller, the more space saved
+
 // New constants are intended to go here
 namespace config
 {
